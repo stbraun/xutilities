53610faeefcfd02b8bdecd01aea97b88eed47283
sb <sb@action.ms>
2020-11-04 03:36:18 +0100

    nox only py3.8

--- noxfile.py
+++ noxfile.py
@@ -7,12 +7,12 @@ import nox
 @nox.session(python=['3.8'])
 def lint(session):
     """Run static analysis."""
-    session.run("pipenv", "install", external=True)
+    session.run("pipenv", "install", "--dev", external=True)
     session.run("flake8", "xutilities/", "tests/")
 
 
-@nox.session(python=['3.7', '3.8'])
+@nox.session(python=['3.8'])
 def tests(session):
     """Run tests for all supported versions of Python."""
-    session.run("pipenv", "install", external=True)
+    session.run("pipenv", "install", "--dev", external=True)
     session.run("pytest", "tests/")
