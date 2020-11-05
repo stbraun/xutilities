c9a120424c8c7f4b31f0887caf77951aaf817701
sb <sb@action.ms>
2020-11-05 13:39:46 +0100

    Setting up travis build.

--- tests/test_sysutils.py
+++ tests/test_sysutils.py
@@ -43,5 +43,11 @@ def test_get_venv():
         path_found = sysutils.get_venv()
     except sysutils.NoVirtualEnvironmentException as e:
         print(e)
+        if e.path is not None:
+            print('Directory listing:')
+            print('-'*40)
+            for item in e.path.glob('*'):
+                print(item)
+            print('-' * 40)
         raise e
     assert isinstance(path_found, Path)
